5785df2a232c4bc2ceff3ce093410f08a7dc9be4
excel <axceldesigns@gmail.com>
2025-08-15 19:52:37 +0100

    task

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Footer from './components/footer/Footer'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col justify-between min-h-screen w-full">
+        <div></div>
+        <div>
+          <Footer />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="min-h-[505px] flex justify-around items-center bg-blue-200 sm:px-[50px] lg:px-[100px]">
+      <div className="h-[419px] bg-amber-200 w-full ">
+      <div>Footer</div>
+      </div>
+    </footer>
+  )
+}
