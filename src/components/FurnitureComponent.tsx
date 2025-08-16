ff6bdb72dc8484bb72476890bcf307c9a32d6dd8
excel <axceldesigns@gmail.com>
2025-08-16 12:35:56 +0100

    task

--- src/components/FurnitureComponent.tsx
+++ src/components/FurnitureComponent.tsx
@@ -1,5 +1,7 @@
 import image from '../assets/img/img'
 
+// declare const image: { [key: string]: string };
+// export default image;
 const FurnitureComponent = () => {
   return (
     <>
@@ -9,16 +11,34 @@ const FurnitureComponent = () => {
           <h1 className="text-4xl font-bold text-gray-800">#FuniroFurniture</h1>
         </div>
         <div className="container mx-auto px-4">
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
-            {Object.values(image).map((imgSrc, i) => (
-              <div key={i} className="overflow-hidden">
-                <img
-                  src={imgSrc as string}
-                  alt={`Funiro furniture setup ${i + 1}`}
-                  className="w-full h-full object-cover hover:scale-105 transition-transform duration-300"
-                />
-              </div>
-            ))}
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 lg:grid-rows-2">
+            <div>
+              <img src={image.img36} alt='36' />
+            </div>
+            <div>
+              <img src={image.img38} alt='38' />
+            </div>
+            <div className='row-span-2 flex justify-center items-center'>
+              <img src={image.img40} alt='40' />
+            </div>
+            <div>
+              <img src={image.img43} alt='43' />
+            </div>
+            <div>
+              <img src={image.img45} alt='45' />
+            </div>
+            <div>
+              <img src={image.img37} alt='37' />
+            </div>
+            <div>
+              <img src={image.img39} alt='39' />
+            </div>
+            <div>
+              <img src={image.img41} alt='41' />
+            </div>
+            <div>
+              <img src={image.img44} alt='44' />
+            </div>
           </div>
         </div>
       </section>
