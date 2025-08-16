39080640c640593ee46c230f1f649e372510a14c
kabir Alaaya <kabiralaaya@gmail.com>
2025-08-16 12:59:16 +0100

    changed title and create routing onmain.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,37 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import BrowseRangeComponent from './components/BrowseRangeComponent'
-import ProductsComponent from './components/ProductsComponent'
-import InspirationComponent from './components/InspirationComponent'
-import Hero from './components/Hero'
-
-import Footer from './components/Footer'
-import FurnitureComponent from './components/FurnitureComponent'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Shop from  './Pages/Shop'
+import Header from "./components/Header";
+import BrowseRangeComponent from "./components/BrowseRangeComponent";
+import ProductsComponent from "./components/ProductsComponent";
+import InspirationComponent from "./components/InspirationComponent";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import FurnitureComponent from "./components/FurnitureComponent";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-       <Header />
-       <Hero />
-       <BrowseRangeComponent />
-       <ProductsComponent />
-       <InspirationComponent />
-       <FurnitureComponent />
-       <Footer />
-    </>
-  )
+    <div className="app" >
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Hero />
+      <BrowseRangeComponent />
+      <ProductsComponent />
+      <InspirationComponent />
+      <FurnitureComponent />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="top-banner">
+        <div className="content">
+          <h1>Home Page</h1>
+         
+        </div>
+      </div>
+      
+      
+    </div>
+  )
+}
