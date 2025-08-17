f9e9a8bbbac016da425a47ce37ce5eecc4262c44
excel <axceldesigns@gmail.com>
2025-08-17 01:32:51 +0100

    task

--- src/components/FurnitureComponent.tsx
+++ src/components/FurnitureComponent.tsx
@@ -8,20 +8,20 @@ const FurnitureComponent = () => {
           <p className="text-xl text-gray-500">Share your setup with</p>
           <h1 className="text-4xl font-bold text-gray-800">#FuniroFurniture</h1>
         </div>
-        <div className="container mx-auto bg-blue-300">
-          <div className="grid max-md:grid-rows-1 max-md:bg-red-300 grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4 bg-green-500">
-            <div className='col-span-2 grid grid-cols-5 grid-rows-2 gap-4'>
-              <img src={image.img36} alt='36' className='col-span-1' />
-              <img src={image.img38} alt='38' className='col-span-4' />
+        <div className="container mx-auto px-4">
+          <div className="grid auto-rows-auto grid-cols-1 md:grid-cols-5 gap-4 place-content-around">
+            <div className='col-span-2 grid grid-cols-5 grid-rows-2 gap-4 justify-items-stretch place-content-around'>
+              <img src={image.img36} alt='36' className='col-span-1 place-self-stretch' />
+              <img src={image.img38} alt='38' className='col-span-4 self-end' />
               <img src={image.img37} alt='37' className='col-span-2' />
               <img src={image.img39} alt='39' className='col-span-3' />
             </div>
-            <div className='flex justify-center items-center'>
+            <div className='hidden md:grid place-content-center'>
               <img src={image.img40} alt='40' />
             </div>
-            <div className='col-span-2 grid grid-cols-6 grid-rows-2 gap-4 bg-amber-300'>
-              <img src={image.img43} alt='43' className='col-span-3' />
-              <img src={image.img45} alt='45' className='col-span-3' />
+            <div className='hidden sm:grid auto-rows-auto col-span-2 grid-cols-6 grid-rows-2 gap-4 justify-items-stretch place-content-around'>
+              <img src={image.img43} alt='43' className='col-span-3 self-end' />
+              <img src={image.img45} alt='45' className='col-span-3 place-self-end' />
               <img src={image.img41} alt='41' className='col-span-2' />
               <img src={image.img44} alt='44' className='col-span-3' />
             </div>
