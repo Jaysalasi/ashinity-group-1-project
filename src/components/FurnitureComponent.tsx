8bb48461d763abd88378c4214e3dc0a2fbea7305
excel <axceldesigns@gmail.com>
2025-08-17 01:57:03 +0100

    task

--- src/components/FurnitureComponent.tsx
+++ src/components/FurnitureComponent.tsx
@@ -9,8 +9,8 @@ const FurnitureComponent = () => {
           <h1 className="text-4xl font-bold text-gray-800">#FuniroFurniture</h1>
         </div>
         <div className="container mx-auto px-4">
-          <div className="grid auto-rows-auto grid-cols-1 md:grid-cols-5 gap-4 place-content-around">
-            <div className='col-span-2 grid grid-cols-5 grid-rows-2 gap-4 justify-items-stretch place-content-around'>
+          <div className="grid  grid-cols-1 md:grid-cols-5 gap-4">
+            <div className='grid content-start col-span-2 grid-cols-5 grid-rows-2 gap-4 justify-items-stretch'>
               <img src={image.img36} alt='36' className='col-span-1 place-self-stretch' />
               <img src={image.img38} alt='38' className='col-span-4 self-end' />
               <img src={image.img37} alt='37' className='col-span-2' />
@@ -19,7 +19,7 @@ const FurnitureComponent = () => {
             <div className='hidden md:grid place-content-center'>
               <img src={image.img40} alt='40' />
             </div>
-            <div className='hidden sm:grid auto-rows-auto col-span-2 grid-cols-6 grid-rows-2 gap-4 justify-items-stretch place-content-around'>
+            <div className='hidden sm:grid content-start col-span-2 grid-cols-6 grid-rows-2 gap-4 justify-items-stretch bg-red-300'>
               <img src={image.img43} alt='43' className='col-span-3 self-end' />
               <img src={image.img45} alt='45' className='col-span-3 place-self-end' />
               <img src={image.img41} alt='41' className='col-span-2' />
